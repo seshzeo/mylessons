f64b7088a4d8eac2f8312be61746658c42b74eb2
seshzeo <serzhnazar09@yandex.ru>
2023-02-03 15:56:24 +0300

    updated lesson3_8

--- lesson3_8/lesson3_8.cpp
+++ lesson3_8/lesson3_8.cpp
@@ -2,9 +2,10 @@
 
 int main()
 {
-    float n = 0, min=3.402823466E+38, max = 0, sum = 0, avrg = 0, val = 0;
+    float n = 0, min = 0, max = 0, sum = 0, avrg = 0, val = 0;
     printf("Введите количество элементов, которые будете воодить по очереди: ");
     scanf("%f", &n);
+
 
     if(n < 1)
     {
@@ -12,15 +13,20 @@ int main()
     }
     else
     {
-        for (int i = 0; i < n; i++)
+        printf("Введите значение: ");
+        scanf("%f", &val);
+        min = max = val;
+        sum += val;
+        for (int i = 1; i < n; i++)
         {
+            
             printf("Введите значение: ");
             scanf("%f", &val);
-            sum = sum + val;
             if(val < min)
                 min = val;
             if(val > max)
                 max = val;
+            sum += val;
         }
     avrg = sum/n;
     printf("Минимальное значение перечня элементов: %f\nМаксимальное значение: %f\nСумма значений: %f\nСреднее арифметическое: %f\n", min,max,sum,avrg); 
